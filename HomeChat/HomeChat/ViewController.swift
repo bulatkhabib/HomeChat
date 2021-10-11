a3426c32c12e148ca8d468a800973a7cb5292419
bulatkhabib <khabibullin23@mail.ru>
2021-10-11 11:49:42 +0300

    [init] project

---
+++ HomeChat/HomeChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeChat
+//
+//  Created by Булат Хабибуллин on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
